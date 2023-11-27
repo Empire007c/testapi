d0c20a39cc28bfde5d9333fab2e0a2d2d001195d
Empire007c <89056941+Empire007c@users.noreply.github.com>
2023-11-27 20:41:19 +0530

    main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def read_root():
 def get_data(symbol: str, limit: int):
 
     data = client.futures_klines(symbol=symbol, interval=client.KLINE_INTERVAL_1MINUTE, limit=5)
-    #data = [[26732.39, 26737.88, 26760.72, 26765.6, 26757.23], [26706.44, 26730.25, 26735.17, 26757.22, 26745.0], [26732.38, 26735.18, 26760.71, 26757.22, 26746.11]]
+    data = [[26732.39, 26737.88, 26760.72, 26765.6, 26757.23], [26706.44, 26730.25, 26735.17, 26757.22, 26745.0], [26732.38, 26735.18, 26760.71, 26757.22, 26746.11]]
 
-    url = "https://baa1-35-201-190-150.ngrok.io/"
+    url = "https://qwertey-bd35-85.hf.space/"
     path = "pattern1/"
 
     data = {"data": data, "ratio": 3}
