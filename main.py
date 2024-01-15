2e5fc985deafa017cf220efe87625f4134fc8aaf
Empire007c <89056941+Empire007c@users.noreply.github.com>
2024-01-15 14:17:15 +0530

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from fastapi import FastAPI
 import binance
 import requests
-from pyngrok import ngrok
 import time
 #import nest_asyncio
 #from threading import Thread
@@ -12,10 +11,6 @@ client = binance.Client()
 
 app = FastAPI()
 
-auth="2aZeouhaHuJSXZQ9WatZC3LQieO_6ckrQG71twNRutGxaLh3H"
-ngrok.set_auth_token(auth)
-public_url = ngrok.connect(8000)
-print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}/\"".format(public_url, 8000))
 
 @app.get("/")
 def read_root():
@@ -27,7 +22,7 @@ def get_data(symbol: str, limit: int):
     data = client.futures_klines(symbol=symbol, interval=client.KLINE_INTERVAL_1MINUTE, limit=5)
     data = [[26732.39, 26737.88, 26760.72, 26765.6, 26757.23], [26706.44, 26730.25, 26735.17, 26757.22, 26745.0], [26732.38, 26735.18, 26760.71, 26757.22, 26746.11]]
 
-    url = "https://qwertey-bd35-85.hf.space/"
+    url = "https://7e11-167-235-8-251.ngrok-free.app"
     path = "pattern1/"
 
     data = {"data": data, "ratio": 3}
